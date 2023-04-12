aa5509828490e74e4960d78798f4354ddd80849c
jhdl0157 <jhdl0157@naver.com>
2023-04-12 22:14:22 +0900

    톰캣 서블릿 컨테이너 생성 완료

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -1,11 +1,17 @@
 package com.example.demo;
 
 
-
+import org.apache.catalina.startup.Tomcat;
+import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
+import org.springframework.boot.web.server.WebServer;
+import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
 
 public class DemoApplication {
 
 	public static void main(String[] args) {
+		ServletWebServerFactory serverFactory = new TomcatServletWebServerFactory();
+		WebServer webServer=serverFactory.getWebServer();
+		webServer.start();
 
 	}
 
