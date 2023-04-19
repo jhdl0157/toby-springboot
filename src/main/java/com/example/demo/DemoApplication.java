e92185cf099928cea519dc17e4e6efadbe7a97e8
Jaeho <jhdl0157@naver.com>
2023-04-19 17:40:43 +0900

    dispatcherservlet만들어서 코드 더 간결하게 만들기

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -11,6 +11,8 @@ import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
+import org.springframework.web.context.support.GenericWebApplicationContext;
+import org.springframework.web.servlet.DispatcherServlet;
 
 import javax.servlet.ServletContext;
 import javax.servlet.ServletException;
@@ -23,7 +25,7 @@ public class DemoApplication {
 
 	public static void main(String[] args) {
 		//스프링 컨테이너 만들기
-		GenericApplicationContext applicationContext = new GenericApplicationContext();
+		GenericWebApplicationContext applicationContext = new GenericWebApplicationContext();
 		applicationContext.registerBean(HelloController.class);
 		applicationContext.registerBean(SimpleHelloService.class);
 		applicationContext.refresh(); //컨테이너를 초기화 해주는 작업
@@ -32,24 +34,9 @@ public class DemoApplication {
 
 		ServletWebServerFactory serverFactory = new TomcatServletWebServerFactory();
 		WebServer webServer=serverFactory.getWebServer(servletContext -> {
-			servletContext.addServlet("frontController",new HttpServlet() {
-				@Override
-				protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException{
-					//인증, 보안, 다국어, 공통기능들을 처리
-					if(req.getRequestURI().equals("/hello") && req.getMethod().equals(HttpMethod.GET.name())) {
-						String name = req.getParameter("name");
-
-						HelloController helloController=applicationContext.getBean(HelloController.class);
-						String ret = helloController.hello(name);
-						
-						resp.setContentType(MediaType.TEXT_PLAIN_VALUE);
-						resp.getWriter().println(ret);
-					}
-					else{
-						resp.setStatus(HttpStatus.NOT_FOUND.value());
-					}
-				}
-			}).addMapping("/*"); //모든 요청을 처리하겠다.
+			servletContext.addServlet("dispatcherServelt",
+					new DispatcherServlet(applicationContext)
+				).addMapping("/*"); //모든 요청을 처리하겠다.
 
 		});
 		webServer.start();
--- src/main/java/com/example/demo/HelloController.java
+++ src/main/java/com/example/demo/HelloController.java
@@ -3,19 +3,25 @@ package com.example.demo;
 import java.util.Objects;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
-
+@RequestMapping("/hello")
 public class HelloController {
 
-    //주입하는 과정
+    // 주입하는 과정
     private final HelloService helloService;
 
     public HelloController(HelloService helloService) {
         this.helloService = helloService;
     }
 
-    public String hello(String name){
-        return helloService.sayHello(Objects.requireNonNull(name)); 
+    @GetMapping
+    @ResponseBody //디스패쳐 서블릿은 Return 타입이 String이면 뷰를 우선적으로 찾느다.
+    public String hello(String name) {
+        return helloService.sayHello(Objects.requireNonNull(name));
     }
 }
