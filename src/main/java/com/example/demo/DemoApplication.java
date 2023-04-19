7b4018241f13576055f8410ec19c00f9c90b1120
Jaeho <jhdl0157@naver.com>
2023-04-19 16:36:02 +0900

    기존 서블릿으로만 작업을 스프링 컨테이너를 만들어서 로직 위임 하기

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -6,6 +6,7 @@ import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactor
 import org.springframework.boot.web.server.WebServer;
 import org.springframework.boot.web.servlet.ServletContextInitializer;
 import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
+import org.springframework.context.support.GenericApplicationContext;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.HttpStatus;
@@ -21,9 +22,15 @@ import java.io.IOException;
 public class DemoApplication {
 
 	public static void main(String[] args) {
+		//스프링 컨테이너 만들기
+		GenericApplicationContext applicationContext = new GenericApplicationContext();
+		applicationContext.registerBean(HelloController.class);
+		applicationContext.refresh(); //컨테이너를 초기화 해주는 작업
+
+
+
 		ServletWebServerFactory serverFactory = new TomcatServletWebServerFactory();
 		WebServer webServer=serverFactory.getWebServer(servletContext -> {
-			HelloController helloController=new HelloController();
 			servletContext.addServlet("frontController",new HttpServlet() {
 				@Override
 				protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException{
@@ -31,15 +38,11 @@ public class DemoApplication {
 					if(req.getRequestURI().equals("/hello") && req.getMethod().equals(HttpMethod.GET.name())) {
 						String name = req.getParameter("name");
 
+						HelloController helloController=applicationContext.getBean(HelloController.class);
 						String ret = helloController.hello(name);
 						
-						resp.setStatus(HttpStatus.OK.value());
-						resp.setHeader(HttpHeaders.CONTENT_TYPE, MediaType.TEXT_PLAIN_VALUE);
-						
+						resp.setContentType(MediaType.TEXT_PLAIN_VALUE);
 						resp.getWriter().println(ret);
-					}
-					else if (req.getRequestURI().equals("/user")){
-
 					}
 					else{
 						resp.setStatus(HttpStatus.NOT_FOUND.value());
