7f8626eff90e768822251a987b3e0c3ed5ab7741
Jaeho <jhdl0157@naver.com>
2023-04-21 12:06:14 +0900

    @Bean 을 사용하엿 빈 오브젝트를 등록하는 방법

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -1,15 +1,19 @@
 package com.example.demo;
 
 import org.apache.catalina.startup.Tomcat;
+import org.springframework.beans.factory.annotation.Configurable;
 import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
 import org.springframework.boot.web.server.WebServer;
 import org.springframework.boot.web.servlet.ServletContextInitializer;
 import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.context.support.GenericApplicationContext;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
+import org.springframework.web.context.support.AnnotationConfigWebApplicationContext;
 import org.springframework.web.context.support.GenericWebApplicationContext;
 import org.springframework.web.servlet.DispatcherServlet;
 
@@ -20,11 +24,21 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
+@Configuration
 public class DemoApplication {
+	//Factory 메서드를 통해서 빈 오브젝트를 만든다.
+	@Bean
+	public HelloController helloController(HelloService helloService){
+		return new HelloController(helloService);
+	}
+	@Bean
+	public HelloService helloService(){
+		return new SimpleHelloService();
+	}
 
 	public static void main(String[] args) {
 		// 스프링 컨테이너 만들기
-		final GenericWebApplicationContext applicationContext = new GenericWebApplicationContext() {
+		 AnnotationConfigWebApplicationContext applicationContext = new AnnotationConfigWebApplicationContext() {
 			@Override
 			protected void onRefresh() {
 				super.onRefresh();
@@ -39,8 +53,7 @@ public class DemoApplication {
 			}
 
 		};
-		applicationContext.registerBean(HelloController.class);
-		applicationContext.registerBean(SimpleHelloService.class);
+		applicationContext.register(DemoApplication.class);  //여기서 출발을 해서 빈 오브젝틀 만들어줘
 		applicationContext.refresh(); // 컨테이너를 초기화 해주는 작업
 
 	}
